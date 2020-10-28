8dba588733a5f9da5cf81d8f45456d492f607994
Santiago Solana <santiago.sjs@gmail.com>
2020-10-28 11:54:58 -0400

    Updates

---
+++ precache-manifest.b5b35285eefdc4ca1a7b388e99c1bca2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5c63946742edf188858be5b0e769063",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "9e18305cbf5e7140e6a1",
+    "url": "/Employee-Directory/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "501ce8537258413d8355",
+    "url": "/Employee-Directory/static/css/main.1982d18d.chunk.css"
+  },
+  {
+    "revision": "9e18305cbf5e7140e6a1",
+    "url": "/Employee-Directory/static/js/2.042ec11a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Employee-Directory/static/js/2.042ec11a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "501ce8537258413d8355",
+    "url": "/Employee-Directory/static/js/main.bbe31f83.chunk.js"
+  },
+  {
+    "revision": "21ab4dbde2f8e2bfdf72",
+    "url": "/Employee-Directory/static/js/runtime-main.965eecad.js"
+  }
+]);
